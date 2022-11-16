fc5f433ce11a0acee3cb2a002341d102bb58db56
Shaxzod <painloversgame@gmail.com>
2022-11-16 16:52:40 +0500

    Forgot password

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
@@ -9,6 +10,10 @@ urlpatterns = [
     path("projects/", include("projects.urls")),
     path("", include("users.urls")),
     path("api/", include("api.urls")),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="reset_password.html"),name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="reset_password_sent.html"),name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="reset.html"),name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="reset_password_complete.html"),name="password_reset_complete"),
 ]
 
 
